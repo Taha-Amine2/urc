c96906ed7c26bdfc645ad9413a7905f57392e117
Taha Amine <tahaamine2002@gmail.com>
2024-11-23 15:42:41 +0100

    Correctifs

--- api/messages.js
+++ api/messages.js
@@ -48,7 +48,7 @@ export default async (request, response) => {
                             icon: "https://www.univ-brest.fr/themes/custom/ubo_parent/favicon.ico",
                         },
                         data: {
-                            senderId: user.id,
+                            senderId: sender.id,
                             receiverId: receiver_id,
                             receiverType: receiver_type,
                             messageId: message.message_id,
